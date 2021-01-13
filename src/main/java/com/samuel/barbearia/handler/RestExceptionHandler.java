f8188264b39573fd1105dcec0755d85d0dd33ed5
SamukaAlves000 <60469274+SamukaAlves000@users.noreply.github.com>
2021-01-13 15:47:33 -0300

    Criando handler global

---
+++ src/main/java/com/samuel/barbearia/handler/RestExceptionHandler.java
@@ -0,0 +1,26 @@
+package com.samuel.barbearia.handler;
+
+import com.samuel.barbearia.exception.BadRequestException;
+import com.samuel.barbearia.exception.BadRequestExceptionDetails;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.time.LocalDateTime;
+
+@ControllerAdvice
+public class RestExceptionHandler {
+    @ExceptionHandler(BadRequestException.class)
+    public ResponseEntity<BadRequestExceptionDetails> handlerBadRequestException (BadRequestException bre){
+        return new ResponseEntity<>(
+            BadRequestExceptionDetails.builder()
+                .timestamp(LocalDateTime.now())
+                .status(HttpStatus.BAD_REQUEST.value())
+                .title("Bad Request exception, Check the documentation")
+                .details(bre.getMessage())
+                .developerMessage(bre.getClass().getName())
+                .build(), HttpStatus.BAD_REQUEST);
+
+    }
+}
